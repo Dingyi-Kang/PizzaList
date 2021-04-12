0c4dcb5b45d7bf3b97a640aec20e54b7db6f795c
Dingyi Kang <81428296+Dingyi-Kang@users.noreply.github.com>
2021-04-12 16:49:46 -0500

    Initial Commit

---
+++ PizzaList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PizzaList
+//
+//  Created by Dingyi Kang on 4/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
